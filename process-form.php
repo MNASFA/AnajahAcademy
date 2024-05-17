6edd1484470dd812c1bbbc5105fcac07290dcceb
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2024-05-17 16:22:35 +0100

    Add files via upload

---
+++ process-form.php
@@ -0,0 +1,43 @@
+<?php 
+    
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $message = $_POST["message"];
+            
+           
+        
+           
+	
+        if(!empty($name) && !empty($email) && !empty($phone)){
+            $link = mysqli_connect("localhost" , "root" , "root" , "anajah_academydb");
+            // $link = mysqli_connect("server217" , "anajahac_user" , "Anajah@24" , "anajahac_db");
+
+            if($link == false){
+                die(mysqli_connect_error());
+            }
+            $sql = "INSERT INTO clients (Name , Email , Phone) values ('$name' , '$email' , '$phone')";
+            //send email
+            include 'send.php';
+            if(mysqli_query($link , $sql)){
+                // echo("Record inserted succesfully...");
+                echo '<script>
+                    window.location.href = "pages/thanks.html";
+                </script>';
+ 
+            }
+            else {
+                echo '<script>
+                    window.location.href = "pages/Error.html";
+                </script>';
+            }
+
+        }else{
+            echo '<script>alert("Remplir Tout les champs !!!!!")
+                    window.location.href = "pages/contact.php"; 
+            </script>';
+            
+        }
+   
+
+?>
